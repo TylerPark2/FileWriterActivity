35131a600edcb93c4a48a93590cdfdb647da61ae
Tyler Park <106443201+TylerPark2@users.noreply.github.com>
2024-09-06 14:41:34 -0700

    Update SillyClass.java
    Added an extra special message for my friend

--- SillyClass.java
+++ SillyClass.java
@@ -1,6 +1,7 @@
 public class SillyClass {
     public static void main (String[]args) {
         System.out.println("Hello, World");
+        System.out.println("This sure is a silly class!");
 
     }
 }
