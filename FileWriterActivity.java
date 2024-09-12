18a959abf125976fa64056de03523d2bf3409d6c
TylerPark3 <tylerhyunpark@gmail.com>
2024-09-11 18:46:23 -0700

    File methods
    Created hidden file method and a method that created a regular file within a hidden folder

---
+++ FileWriterActivity.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterActivity {
+    
+    public void createHiddenFileWithPassword(String password) {
+        File hiddenFile = new File(".huluPassword.txt");
+
+        try {
+            if (!hiddenFile.exists()) {
+                hiddenFile.createNewFile();
+            }
+
+            FileWriter writer = new FileWriter(hiddenFile);
+            writer.write(password);
+            writer.close();
+
+            System.out.println("Hidden file '.huluPassword.txt' created successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createFileInHiddenFolder(String topSecretData) {
+        File hiddenFolder = new File(".uniqueClassifiedFolder");
+        File regularFile = new File(hiddenFolder, "regularFile.dat");
+
+        try {
+            if (!hiddenFolder.exists()) {
+                hiddenFolder.mkdir();
+                System.out.println("Hidden folder '.uniqueClassifiedFolder' created successfully.");
+            }
+
+            if (!regularFile.exists()) {
+                regularFile.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(regularFile);
+            fw.write(topSecretData);
+            fw.close();
+
+            System.out.println("File 'regularFile.dat' created inside hidden folder '.uniqueClassifiedFolder' successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FileWriterActivity fileWriterActivity = new FileWriterActivity();
+
+        String password = "SuperSecretPassword!";
+        fileWriterActivity.createHiddenFileWithPassword(password);
+
+        String topSecretData = "This is top-secret information.";
+        fileWriterActivity.createFileInHiddenFolder(topSecretData);
+    }
+}
